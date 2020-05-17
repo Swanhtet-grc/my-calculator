cf88e9fc8ae968e9704c4ad3cbd694ed1c8b92af
Swan Htet <57400158+Swanhtet-grc@users.noreply.github.com>
2020-05-17 00:50:19 -0700

    Delete Title.js

--- Title.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import "./Title.css"
-const title = (props) => {
-    return (
-        <div className="Title"><h1>Welcome to Swan's Calculator</h1></div>
-    )
-}
-
-export default title;
