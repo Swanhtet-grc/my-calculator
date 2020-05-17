834d34fc83dc981848266612b282c34df2f816e5
Swan Htet <57400158+Swanhtet-grc@users.noreply.github.com>
2020-05-17 00:48:44 -0700

    Delete Operator.js

--- Operator.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./Card.css"
-const operator = (props) => {
-    return (
-        <div className="Card" onClick={() => { props.click() }}>{props.num}</div>
-    )
-}
-
-export default operator;
