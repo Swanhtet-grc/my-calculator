044976ebf5601bf05ba23ac991ae6f9a519289ae
Swanhtet-grc <swanhtet.grc@gmail.com>
2020-06-01 09:13:18 -0500

    v3

--- src/Container/Container.js
+++ src/Container/Container.js
@@ -6,13 +6,13 @@ import Result from "./Result";
 class Container extends Component {
     state = {
         result: [
-            { value: 0 }
-        ]
+            { value: 0 }]
     }
+
     displayList = "";
     reg1 = /[0-9]/;
     reg2 = /[÷x+-]/;
-    reg3 = /[[÷x+-][0-9]/;
+    reg3 = /[[÷x+-][0-9.]/;
     operatorList = ["÷", "x", "+", "-"]
     operatorCounter = 0;
     pointCounter = 1;
@@ -46,9 +46,30 @@ class Container extends Component {
 
     }
 
+    clear = () => {
+        this.displayList = "";
+        this.operatorCounter = 0;
+        this.pointCounter = 1;
+        this.setState({ result: [{ value: 0 }] })
+    }
+
+    delete = () => {
+        if (this.displayList.length > 0) {
+            if (this.displayList[this.displayList.length - 1] === ".") {
+                this.pointCounter -= 1
+            } else if (this.operatorList.includes(this.displayList[this.displayList.length - 1])) {
+                this.operatorCounter -= 1
+            }
+            this.displayList = this.displayList.substring(0, this.displayList.length - 1)
+            if (this.displayList.length > 0) {
+                this.setState({ result: [{ value: this.displayList }] })
+            } else {
+                this.setState({ result: [{ value: 0 }] })
+            }
+        }
+    }
+
     operator = () => {
-
-
         for (var i = 0; i < this.displayList.length; i++) {
             if (this.displayList[i] === this.operatorList[0]) {
                 var num = this.displayList.split("÷");
@@ -66,6 +87,13 @@ class Container extends Component {
 
         }
         if (this.reg3.test(this.displayList)) {
+            if (Number.isInteger(answer)) {
+                this.pointCounter = 1
+            } else {
+                this.pointCounter = 2
+            }
+            this.displayList = '' + answer
+            this.operatorCounter = 0
             this.setState({ result: [{ value: answer }] })
         }
 
@@ -74,22 +102,26 @@ class Container extends Component {
         return (
             <div className="Container">
                 <Result value={this.state.result[0].value} click={(num) => this.operator()} />
+                <Card num="&#8730;x" click={() => alert("This fucntion is not available yet!")} />
+                <Card num="x²" click={() => alert("This fucntion is not available yet!")} />
+                <Card num="C" click={() => this.clear()} />
+                <Card num="÷" click={(num) => this.displayChanger(num)} />
                 <Card num="7" click={(num) => this.displayChanger(num)} />
                 <Card num="8" click={(num) => this.displayChanger(num)} />
                 <Card num="9" click={(num) => this.displayChanger(num)} />
-                <Card num="÷" click={(num) => this.displayChanger(num)} />
+                <Card num="x" click={(num) => this.displayChanger(num)} />
                 <Card num="4" click={(num) => this.displayChanger(num)} />
                 <Card num="5" click={(num) => this.displayChanger(num)} />
                 <Card num="6" click={(num) => this.displayChanger(num)} />
-                <Card num="x" click={(num) => this.displayChanger(num)} />
+                <Card num="-" click={(num) => this.displayChanger(num)} />
                 <Card num="1" click={(num) => this.displayChanger(num)} />
                 <Card num="2" click={(num) => this.displayChanger(num)} />
                 <Card num="3" click={(num) => this.displayChanger(num)} />
-                <Card num="-" click={(num) => this.displayChanger(num)} />
-                <Operator num="=" click={() => this.operator()} />
+                <Card num="+" click={(num) => this.displayChanger(num)} />
                 <Card num="0" click={(num) => this.displayChanger(num)} />
                 <Card num="." click={(num) => this.displayChanger(num)} />
-                <Card num="+" click={(num) => this.displayChanger(num)} />
+                <Card num={<img src="delete.png" className="img1"></img>} click={() => this.delete()} />
+                <Operator num="=" click={() => this.operator()} />
             </div>
         )
     }
